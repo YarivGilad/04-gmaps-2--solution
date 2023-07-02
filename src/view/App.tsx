9adf7cb5292143be4e49a33e4d9b3e839c1a14e9
Yariv Gilad <yariv.gilad@gmail.com>
2023-07-02 06:11:53 +0300

    solution 2

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -8,6 +8,9 @@ interface State {
   lat: number;
   lng: number;
   zoom: number;
+  title: string;
+  type: string;
+  markerReady: boolean;
 }
 
 export class App extends Component<object, State> {
@@ -17,11 +20,13 @@ export class App extends Component<object, State> {
     lat: -34.397,
     lng: 150.644,
     zoom: 8,
+    title: "",
+    type: "",
+    markerReady: false,
   };
 
-  // reposition = (event: MouseEvent)=> {
-  //  const city = (event.target as HTMLElement).dataset.city
-  reposition(city: string) {
+  reposition = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const city = (event.target as HTMLElement).dataset.city;
     switch (city) {
       case "tel aviv":
         this.setState({ lat: 32.0042938, lng: 34.7615399 });
@@ -35,7 +40,7 @@ export class App extends Component<object, State> {
       default:
         alert("wrong city");
     }
-  }
+  };
   updateZoom = (event: ChangeEvent) => {
     const z = (event.target as HTMLInputElement).value;
     // const z = (this.input.current as HTMLInputElement).value;
@@ -46,6 +51,61 @@ export class App extends Component<object, State> {
     // this.setState({ zoom: +z }); // parseInt(zoom); // +zoom;
   };
 
+  locate_me = () => {
+    log("position");
+    if ("geolocation" in navigator) {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          log({ position });
+
+          // const lat = position.coords.latitude;
+          // const lng = position.coords.longitude;
+
+          const { latitude: lat, longitude: lng } = position.coords;
+
+          this.setState({ lat, lng, zoom: 18 });
+        },
+        (error) => {
+          log(`ERROR(${error.code}): ${error.message}`);
+        }
+      );
+    } else {
+      alert("geolocation IS NOT available in your browser :(");
+    }
+  };
+
+  // updateField = (event) => {
+  //   log("title", event.target.value);
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   });
+  // };
+
+  updateTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    log("title", event.target.value);
+    this.setState({
+      title: event.target.value
+    });
+  };
+
+  updateType = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    log("type", event.target.value);
+    this.setState({
+      type: event.target.value
+    });
+  };
+
+  addMarker = () => {
+    log("add_marker");
+    log("ready? ", this.state.markerReady);
+    this.setState({ markerReady: true }, () => {
+      log("ready? ", this.state.markerReady);
+      this.setState({ markerReady: false });
+    });
+    // this.setState({ marker_ready: false });
+  };
+
+  
   render() {
     log(this.state);
     return (
@@ -53,22 +113,18 @@ export class App extends Component<object, State> {
         <TopBar>
           <h1>Google Maps Example in React</h1>
         </TopBar>
-        <div className="hbox mb20">
-          <button
-            data-city="tel aviv"
-            onClick={() => this.reposition("tel aviv")}
-          >
+        <div className="controls-box mb20">
+          <span>City: &nbsp;</span>
+          <button data-city="tel aviv" onClick={this.reposition}>
             Tel Aviv
           </button>
-          <button data-city="paris" onClick={() => this.reposition("paris")}>
+          <button data-city="paris" onClick={this.reposition}>
             Paris
           </button>
-          <button
-            data-city="london"
-            onClick={this.reposition.bind(this, "london")}
-          >
+          <button data-city="london" onClick={this.reposition}>
             London
           </button>
+          <span>Zoom: &nbsp;</span>
           <input
             ref={this.input}
             type="number"
@@ -76,14 +132,47 @@ export class App extends Component<object, State> {
             max="16"
             placeholder="8"
             onChange={this.updateZoom}
-          />
+          /> 
+          <button onClick={this.locate_me}>locate me</button>
         </div>
-        <GoogleMap
+        <div className="controls-box mb20">
+          <span>Title: &nbsp;</span>
+          <input
+            name="title"
+            type="text"
+            className="title-input"
+            onChange={this.updateTitle}
+          />
+          <select
+            name="type"
+            className="type-selector"
+            onChange={this.updateType}
+          >
+            <option value="None">Type:</option>
+            <option value="Barbecue">Barbecue </option>
+            <option value="Buffet">Buffet </option>
+            <option value="Brasserie">Brasserie</option>
+            <option value="Cafe">Cafe</option>
+            <option value="Casual">Casual</option>
+            <option value="Chef">Chef</option>
+            <option value="Diner">Diner</option>
+            <option value="Ethnic">Ethnic</option>
+            <option value="Fast food">Fast food</option>
+            <option value="Kosher">Kosher</option>
+            <option value="Pub">Pub</option>
+          </select>
+          <button onClick={this.addMarker}>Add Marker</button>
+        </div>
+        <GoogleMap {...this.state} />
+        {/* <GoogleMap
           lat={this.state.lat}
           lng={this.state.lng}
           zoom={this.state.zoom}
-        />
-        {/* <GoogleMap {...this.state} /> */}
+          title={this.state.title}
+          type={this.state.type}
+          markerReady={this.state.markerReady}
+        /> */}
+      
       </div>
     );
   }
--- src/view/GMaps.tsx
+++ src/view/GMaps.tsx
@@ -3,13 +3,15 @@ import React, { Component } from "react";
 const log = (...args: any[]) => console.log("GoogleMap -->", ...args);
 
 interface Props {
-    lat: number;
-    lng: number;
-    zoom: number;
+  lat: number;
+  lng: number;
+  zoom: number;
+  title: string;
+  type: string;
+  markerReady: boolean;
 }
 
 export class GoogleMap extends Component<Props> {
-
   mapRef = React.createRef<HTMLDivElement>();
   theMap: google.maps.Map | null = null;
 
@@ -21,21 +23,51 @@ export class GoogleMap extends Component<Props> {
     // log("nextProps:", nextProps);
     // log("<<<< shouldComponentUpdate");
     const map = this.theMap as google.maps.Map;
-    map.setCenter({ lat: nextProps.lat, lng: nextProps.lng });
-    map.setZoom(nextProps.zoom);
+
+    if (nextProps.markerReady === true) {
+      this.addMarker(nextProps.title, nextProps.type);
+    }
+    if (nextProps.lat !== this.props.lat || nextProps.lng !== this.props.lng) {
+      map.setCenter({ lat: nextProps.lat, lng: nextProps.lng });
+    }
+    if (this.props.zoom !== nextProps.zoom) {
+      map.setZoom(nextProps.zoom);
+    }
+
     return false;
+  }
+
+  addMarker(title:string, type:string) {
+    const map = this.theMap as google.maps.Map;
+    const marker = new google.maps.Marker({
+      position: map.getCenter(),
+      map,
+      title
+    });
+    const infoWindow = new google.maps.InfoWindow({
+      content: `
+      <div style="text-align:center">
+        <h2>${title}</h2>
+        <h3>Type: ${type}</h3>
+        <br/>
+        <img src="https://picsum.photos/125/85?r=${Math.random()}"/>
+        <br/>
+      </div>`
+    });
+    marker.addListener("click", function () {
+      infoWindow.open(map, marker);
+    });
   }
 
   componentDidMount() {
     // log(this.mapRef);
     this.theMap = new google.maps.Map(this.mapRef.current as HTMLDivElement, {
       center: { lat: this.props.lat, lng: this.props.lng },
-      zoom: 8
+      zoom: 8,
     });
   }
 
   render() {
     return <div ref={this.mapRef} className="map-box" />;
   }
-
 }
